581032943af36140d53ca448827aa3ccdd48583b
uzs7jf <ismael.nieto@gmail.com>
2021-04-21 16:16:31 +0200

    building controller layer

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
-// server.js
-var app = require('./app');
-var port = process.env.PORT || 3000;
-var server = app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
-});
+// SERVER: App entry point
+const app = require("./app");
+//const db = require("./db/DBConnection");
+
+// Setting port
+const port = process.env.PORT || 8080;
+
+app.listen(app.get(port), () => {
+    console.log("Express server started and running on port:" + port);
+  });
+

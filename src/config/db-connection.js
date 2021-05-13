e920173615dc8688d09df8cdb38d8960996b14c5
Ismael Nieto <ismael.nieto@gmail.com>
2021-05-13 12:35:34 +0200

    last configuration changes

--- src/config/db-connection.js
+++ src/config/db-connection.js
@@ -1,14 +1,16 @@
 // Basic database conection with node.js
 // Database connection data comes from db.config.js
 const mysql = require("mysql2");
-const dbConfig = require("./db.config.js");
+//const dbConfig = require("./db.config.js");
+const dotenv = require('dotenv');
+dotenv.config();
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB,
+  host: process.env.PORTHOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB,
 });
 
 // open the MySQL connection
